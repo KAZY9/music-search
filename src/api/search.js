c09d7292afb116625b52c67976e3fbec407efabe
KAZY9 <kazu.m.sora9@gmail.com>
2024-03-22 17:48:35 +0900

    modify music list

--- src/api/search.js
+++ src/api/search.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import ReactAudioPlayer from "react-audio-player";
 import useSpotifyAuth from "./Auth";
 
 const END_POINT = 'https://api.spotify.com/v1/search';
@@ -8,6 +9,8 @@ const SearchSongs = () => {
     const [query, setQuery] = useState('');
     const [songs, setSongs] = useState([]);
     const [message, setMessage] = useState('')
+    const [playingTrackId, setPlayingTrackId] = useState(null);
+
     const token = useSpotifyAuth();
   
     const searchSongs = async () => {
@@ -18,7 +21,7 @@ const SearchSongs = () => {
             return;
       }
       try {
-        const response =  await axios(END_POINT + `?q=${query}&type=track&limit=30&market=JP`, {
+        const response =  await axios(END_POINT + `?q=${query}&type=track&market=UA`, {
           method: 'GET',
           headers: {
               Authorization: `Bearer ${token}`
@@ -30,25 +33,23 @@ const SearchSongs = () => {
         //重複するトラックは個別に評価される
         const soretedSongs = response.data.tracks.items.sort((a, b) => b.popularity - a.popularity);
 
-        // const uniqueSongs = Array.from(new Set(soretedSongs.map(song => song.id))).map(id => {
-        //     return soretedSongs.find(song => song.id === id);
-        // });
-
-        const groupedSongs = soretedSongs.reduce((acc, song) => {
-          if (!acc[song.name]) {
-            acc[song.name] = [];
-          }
-          acc[song.name].push(song);
-          return acc;
-        }, []);
-
-        const uniqueSongs = Object.values(groupedSongs).map(group => {
-          return group.reduce((max, song) => (song.popularity > max.popularity ? song : max), group[0]);
+        const uniqueSongs = Array.from(new Set(soretedSongs.map(song => song.id))).map(id => {
+            return soretedSongs.find(song => song.id === id);
         });
 
-        const first10Songs = uniqueSongs.slice(0, 10);
+        // const groupedSongs = soretedSongs.reduce((acc, song) => {
+        //   if (!acc[song.name]) {
+        //     acc[song.name] = [];
+        //   }
+        //   acc[song.name].push(song);
+        //   return acc;
+        // }, []);
 
-        setSongs(first10Songs);
+        // const uniqueSongs = Object.values(groupedSongs).map(group => {
+        //   return group.reduce((max, song) => (song.popularity > max.popularity ? song : max), group[0]);
+        // });
+
+        setSongs(uniqueSongs);
         setMessage('');
       } catch (error) {
         console.error('Error fetching songs:', error);
@@ -59,6 +60,36 @@ const SearchSongs = () => {
         if (e.key === 'Enter' && e.shiftKey) {
             searchSongs();
         }
+    }
+
+    const handlePlay = (selectedTrackId) => {
+      const audioElements = document.querySelectorAll('audio');
+
+      audioElements.forEach(audio => {
+        if (audio.id !== `track-${selectedTrackId.toString()}`) {
+          audio.pause();
+          audio.currentTime = 0;
+        }
+      });
+
+      setPlayingTrackId(selectedTrackId);
+    }
+
+    const playNextSong = () => {
+      const currentIndex = songs.findIndex(song => song.id === playingTrackId);
+      const nextIndex = currentIndex + 1;
+
+      // if(nextIndex < songs.length) {
+      //   handlePlay(songs[nextIndex].id);
+      // }
+      if (nextIndex < songs.length) {
+        const nextSongId = songs[nextIndex].id;
+        const nextAudioElement = document.getElementById(`track-${nextSongId}`);
+        if (nextAudioElement) {
+          nextAudioElement.play();
+          setPlayingTrackId(nextSongId);
+        }
+      }
     }
   
     return (
@@ -76,11 +107,22 @@ const SearchSongs = () => {
           </thead>
           <tbody>
             {songs.map(song => (
-              <tr key={song.id}>
-                <td>{song.name}</td>
-                <td>{song.artists.map(artist => artist.name)}</td>
-                <td>{song.popularity}</td>
-              </tr>
+                <tr key={song.id}>
+                  <td>
+                    <img src={song.album.images[0].url} alt="Album cover" style={{ width: '50px', height: '50px', marginRight: '10px' }}/>
+                    {song.name}
+                  </td>
+                  <td>{song.artists.map(artist => artist.name).join(', ')}</td>
+                  <td>
+                    <ReactAudioPlayer
+                      id={`track-${song.id}`}
+                      src={song.preview_url}
+                      controls
+                      volume={0.4}
+                      onPlay={() => handlePlay(song.id)}
+                      onEnded={playNextSong} />
+                  </td>
+                </tr>
             ))}
           </tbody>
         </table>
